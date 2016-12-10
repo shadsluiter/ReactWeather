7d042995ed5ab8082a859c0a32c879130b5ce673
shadsluiter <shadsluiter@shads-MacBook-Air.local>
2016-12-10 09:21:49 -0700

    update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,23 +1,12 @@
 var React = require('react');
 
 
-// var Examples = React.createClass({
-//   render: function() {
-//     return(
-//       <div>
-//
-//       <h3>Examples component</h3>
-//     </div>
-//     );
-//   }
-// });
-
 
 var Examples = (props) => {
   return(
     <div>
 
-    <h3>Examples component</h3>
+    <h3>Examples component!</h3>
   </div>
   );
 };
