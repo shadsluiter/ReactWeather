2503038da47d79543f6259f4f0f39b661f41fec2
shadsluiter <shadsluiter@shads-MacBook-Air.local>
2016-12-11 06:04:24 -0700

    installed foundation

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 var fullName = "Shad Sluiter";
 var theMessage = "A message from React";
 ReactDOM.render(
