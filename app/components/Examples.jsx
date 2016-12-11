2fe116d70447b49b23e4ccd69dac903a812618e5
shadsluiter <shadsluiter@shads-MacBook-Air.local>
2016-12-11 07:06:37 -0700

    add grid system style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,20 @@
 var React = require('react');
-
+var { Link } = require('react-router');
 
 
 var Examples = (props) => {
   return(
     <div>
-    <h3>Examples!</h3>
-    <p>Welcome to the examples page</p>
+    <h1 className = "text-center">Examples!</h1>
+    <p>Here are a few example locations to try out</p>
+    <ol>
+      <li>
+        <Link to = "/?location=Phoenix">Phoenix, AZ</Link>
+      </li>
+      <li>
+          <Link to = "/?location=Fairbanks">Fairbanks, AK</Link>
+      </li>
+    </ol>
   </div>
   );
 };
