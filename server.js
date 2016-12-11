f6427c7e8b53ad58c45c34fca9dc0d4dbbaba3a5
shadsluiter <shadsluiter@shads-MacBook-Air.local>
2016-12-10 20:58:28 -0700

    prep repo for heroku

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ var express = require('express');
 // create the app
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+      res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('server started');
+app.listen(PORT, function() {
+  console.log('server started on port ' + PORT);
 
 });
